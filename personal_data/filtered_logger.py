4036e388090f7905ee9ba96a148874eafcb7671b
aliHasanov22 <ali2004hasan2005@gmail.com>
2026-04-02 15:25:51 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""encription intrudaction personal data"""
+"""Filtered logger module."""
+
 import re
 from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """return the log message obfuscated"""
-
-    return re.sub(f"({'|'.join(fields)})=[^{separator}]*",
-                  f"\\1={redaction}", message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates specified fields in a log message."""
+    return re.sub(
+        rf"({'|'.join(fields)})=[^{separator}]*",
+        lambda m: f"{m.group(1)}={redaction}",
+        message
+    )
