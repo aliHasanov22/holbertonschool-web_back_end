ae3de575fc2e183712e3403712d70c72c2abef2b
root <ali2004hasan2005@gmail.com>
2026-04-04 20:56:41 +0400

    data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,19 @@ Filtered logger module.
 """
 
 import logging
+import re
 from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Return the log message with specified fields obfuscated.
+    """
+    for field in fields:
+        message = re.sub(rf"{field}=[^{separator}]*",
+                         f"{field}={redaction}", message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
@@ -18,13 +30,14 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         """
-        Initialize the formatter with fields to redact.
+        Initialize formatter with fields to redact.
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        Format a log record.
+        Return formatted log record with sensitive fields redacted.
         """
-        raise NotImplementedError
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
