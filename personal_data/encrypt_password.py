cd7654bc31fdbe31b9150a7d09d82587984a1d81
root <ali2004hasan2005@gmail.com>
2026-04-04 21:15:55 +0400

    data

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for hashing passwords with bcrypt.
+Encrypt password using bcrypt
 """
 
 import bcrypt
@@ -8,6 +8,15 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """
-    Return a salted, hashed password as a byte string.
+    Hashes a password using bcrypt with a generated salt.
     """
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against a hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
