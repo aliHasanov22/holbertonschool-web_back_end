7c8d2ac4a60fcbf2c1be36b7bb839ec6182449fb
unknown <ali2004hasan2005@gmail.com>
2026-04-08 01:18:38 +0400

    endpoint

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,9 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized', methods=['GET'])
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
+def unauthorized():
+    """
       - the unauthorized error
     """
     abort(401)
