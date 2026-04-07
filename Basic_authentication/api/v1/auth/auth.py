f254437bec5077766cd55811cf7b29c83c88a24d
unknown <ali2004hasan2005@gmail.com>
2026-04-08 02:29:30 +0400

    auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,17 +6,14 @@ from typing import List, TypeVar
 
 class Auth:
     """Auth class to manage the API authentication."""
-   
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth method to determine if the path is protected."""
         return False
-    
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header method to get the auth header from the request."""
+        """Authorization header method to get the auth header from request."""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method to get the current user from the request."""
