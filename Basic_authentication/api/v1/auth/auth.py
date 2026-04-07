88d280aef577849ab19f33b0f7260c65c3e8e5d2
unknown <ali2004hasan2005@gmail.com>
2026-04-08 02:18:19 +0400

    auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+import requests from flask
+from typing import List, TypeVar
+""" Module of API views"""
+
+
+class Auth:
+    """Auth class to manage the API authentication."""
+   
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth method to determine if the path is protected."""
+        return False
+    
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header method to get the auth header from the request."""
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user method to get the current user from the request."""
+        return None
