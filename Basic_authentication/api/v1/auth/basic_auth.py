5ef5e3b9ba636372819eec1b70dc1d8ad85e3c9c
unknown <ali2004hasan2005@gmail.com>
2026-04-08 17:23:55 +0400

    basic_auth_update

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,17 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+            """Decode base64 string to utf-8 string"""
+            if base64_authorization_header is None:
+                return None
+            if not isinstance(base64_authorization_header, str):
+                return None
+            try:
+                decoded_bytes = base64.b64decode(base64_authorization_header)
+                decoded_str = decoded_bytes.decode('utf-8')
+                return decoded_str
+            except Exception:
+                return None
