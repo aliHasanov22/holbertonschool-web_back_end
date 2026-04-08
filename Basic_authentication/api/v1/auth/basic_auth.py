9224835aa85b061e4d51ced9fa03985cf569104e
unknown <ali2004hasan2005@gmail.com>
2026-04-08 16:32:49 +0400

    basic_auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+!#/usr/bin/env python3
+""" Module of API views for the API authentication. """
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth is a class inherited from Auth to
+       manage Api authentication"""
+    pass
