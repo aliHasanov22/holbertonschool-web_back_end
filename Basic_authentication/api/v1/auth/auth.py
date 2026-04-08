002f130dc850c87599710d29bce53c687a7c823c
unknown <ali2004hasan2005@gmail.com>
2026-04-08 16:15:39 +0400

    auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header method to get the auth header from request."""
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user method to get the current user from the request."""
