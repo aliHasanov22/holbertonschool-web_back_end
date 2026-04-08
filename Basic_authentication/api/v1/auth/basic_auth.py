eecda5026c389dd61d106d791934ba5cd9404f0d
unknown <ali2004hasan2005@gmail.com>
2026-04-08 17:14:06 +0400

    basic_auth_update

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,12 +6,18 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth is a class inherited from Auth to manage Api authentication"""
-   def extract_base64_authorization_header(self,
-                                           authorization_header: str) -> str:
-      """Extract base64 part of auth header for basic auth"""
-      if authorization_header is None  or type(authorization_header) != str:
-         return None
-      if not authorization_header.startswith('Basic '):
-         return None
-      return authorization_header[6:]
+    """BasicAuth class that inherits from Auth"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Extract base64 part of Authorization header"""
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header[6:]
