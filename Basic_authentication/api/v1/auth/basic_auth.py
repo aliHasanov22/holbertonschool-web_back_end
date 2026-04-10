10fe4036b9bfcee4f9d4932c7da44a9517635925
unknown <ali2004hasan2005@gmail.com>
2026-04-10 10:55:22 +0400

    basic_auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,9 +39,10 @@ class BasicAuth(Auth):
 
         except Exception:
             return None
-    
+
     def extract_user_credentials(self,
-            decoded_base64_authorization_header: str) -> (str, str):
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
         """operate the user credebtials such as username and password"""
         if decoded_base64_authorization_header is None or not isinstance(
                 decoded_base64_authorization_header, str):
