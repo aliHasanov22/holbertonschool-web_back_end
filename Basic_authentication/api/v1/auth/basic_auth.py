80e3ce88d1075455d3a9a8edc29739d8eef8319b
unknown <ali2004hasan2005@gmail.com>
2026-04-10 11:24:31 +0400

    basic_auth_UserObject

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,6 +53,7 @@ class BasicAuth(Auth):
 
         user_cre = decoded_base64_authorization_header.split(':', 1)
         return user_cre[0], user_cre[1]
+
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """ returns the User instance based on his email and password"""
