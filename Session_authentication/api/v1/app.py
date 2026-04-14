a1cd3e8cef5943b6c198729ddc4bdd94cb2c0566
unknown <ali2004hasan2005@gmail.com>
2026-04-14 16:40:02 +0400

    session_auth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -16,7 +16,9 @@ auth = None
 if getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+elif getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 @app.before_request
 def before_request():
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module of API views for the API authentication. """
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """SessionAuth class that inherits from Auth"""
+    pass
