a0d82840716add1c40519c1546c5a8944549210a
unknown <ali2004hasan2005@gmail.com>
2026-04-14 17:35:28 +0400

    session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,7 +12,7 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """"Create session id for user_id"""
-        if user_id is None or not isinstance(user_id,str):
+        if user_id is None or not isinstance(user_id, str):
             return None
 
         session_id = str(uuid.uuid4())
