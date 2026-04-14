f3b6f7717429744f500993f10feeb69b84dc8656
unknown <ali2004hasan2005@gmail.com>
2026-04-14 17:40:51 +0400

    session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,8 +20,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """return user id according to session id"""
-        
+        """return user id according to session id""" 
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
