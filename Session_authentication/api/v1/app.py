0f0acc1727a5da2a20cb30fbd73d858ecc699ca3
unknown <ali2004hasan2005@gmail.com>
2026-04-14 16:27:11 +0400

    session_auth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -29,6 +29,8 @@ def before_request():
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
+
+        request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
             abort(403)
 
