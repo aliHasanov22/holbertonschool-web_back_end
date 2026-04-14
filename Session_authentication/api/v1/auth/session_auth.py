1cc5b1025a8e4514129d1124fc9b73636212c922
unknown <ali2004hasan2005@gmail.com>
2026-04-14 16:49:11 +0400

    session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
     """SessionAuth class that inherits from Auth"""
     pass
