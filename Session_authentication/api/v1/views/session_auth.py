1a3c3a2e6aa6499ecc7e618d94b647eda32efb11
unknown <ali2004hasan2005@gmail.com>
2026-04-17 18:09:35 +0400

    auth

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-""" Module of API views for the API authentication. """
+"""Module of API views for Session Authentication"""
+
 import os
-from flask import app, jsonify, request
-from typing import List, TypeVar
-from Session_authentication.api.v1 import auth
-import json
+from flask import jsonify, request
+from models.user import User
+from api.v1.views import app_views
 
 
-@app.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login', methods=['POST'],
+                 strict_slashes=False)
 def login():
-    """login route for session auth"""
-    from models.user import User
+    """Login route for session authentication"""
     email = request.form.get('email')
     password = request.form.get('password')
 
     if email is None or email == "":
-        return json.jsonify({"error": "email missing"}), 400
+        return jsonify({"error": "email missing"}), 400
 
     if password is None or password == "":
-        return json.jsonify({"error": "password missing"}), 400
+        return jsonify({"error": "password missing"}), 400
 
     try:
-        Users = User.search({'email': email})
+        users = User.search({'email': email})
     except Exception:
-        Users = []
+        users = []
 
-    if not Users:
-        return json.jsonify({"error": "no user found for this email"}), 404
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
 
-    user = Users[0]
+    user = users[0]
     if not user.is_valid_password(password):
-        return json.jsonify({"error": "wrong password"}), 401
+        return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
