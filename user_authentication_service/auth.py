9aff5ecd8619280851d380f8998654f11e04fa85
unknown <ali2004hasan2005@gmail.com>
2026-04-22 14:52:29 +0400

    user auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,8 +3,24 @@
 from db import DB
 from user import User
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """hashed password"""
     return hashpw(password.encode(), gensalt())
+
+class Auth:
+    """Auth class to interact with auth data"""
+
+    def __init__(self):
+        """initialize auth class"""
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """register user to the database"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
