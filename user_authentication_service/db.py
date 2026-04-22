8c00fe14fdc31de11f637767dd43ad6205d864e7
unknown <ali2004hasan2005@gmail.com>
2026-04-22 14:38:51 +0400

    user auth

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -40,3 +40,12 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update user"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
