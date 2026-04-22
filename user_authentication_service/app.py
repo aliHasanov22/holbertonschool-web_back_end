3ea9f503af0854fe78c61218ba2e2eb2dd322a83
unknown <ali2004hasan2005@gmail.com>
2026-04-22 15:25:10 +0400

    user auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -14,5 +14,19 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """register user to the database"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(400)
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
