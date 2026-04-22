6bbca8b9936571d914042cb43bc123d5cb6eef1f
unknown <ali2004hasan2005@gmail.com>
2026-04-22 14:27:56 +0400

    user auth

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """find user0"""
         user = self._session.query(User).filter_by(**kwargs).first()
