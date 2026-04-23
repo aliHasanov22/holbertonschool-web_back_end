9a5af41b5950f5312f2be048745b6902461afa58
unknown <ali2004hasan2005@gmail.com>
2026-04-23 09:17:43 +0400

    user auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, abort
 from auth import Auth
 
-
 app = Flask(__name__)
 AUTH = Auth()
 
@@ -26,6 +25,22 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """login user and create session"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(400)
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
