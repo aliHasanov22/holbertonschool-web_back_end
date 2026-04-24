f1daaf4b592bf4a9e98887c00ea27e0c3a9f06ae
unknown <ali2004hasan2005@gmail.com>
2026-04-25 00:14:24 +0400

    user auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -64,5 +64,18 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """get reset pass token"""
+    email = request.form.get('email')
+    if not email:
+        abort(400)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
