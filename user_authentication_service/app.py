e8fca9b8460b0e33f024da9ee61de99b49e90402
unknown <ali2004hasan2005@gmail.com>
2026-04-24 23:18:09 +0400

    user auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -25,7 +25,7 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
- 
+
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
