25cb9f8ec0837221a1b4db5077f689638263773c
unknown <ali2004hasan2005@gmail.com>
2026-05-02 15:36:17 +0400

    SQL advanced

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- sql for calcualting bands with glam rock
 SELECT band_name, 
-       IFNULL(2024 - split,2024) -formed AS lifespan
+       IFNULL(2024 - split, 2024) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%glam rock%'
 ORDER BY lifespan DESC;
